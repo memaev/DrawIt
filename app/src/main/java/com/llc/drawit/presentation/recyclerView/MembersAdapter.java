ba2dc6cf8c431f77488766ca709522912ab6d9f5
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2025-06-21 20:22:05 +0300

    Display members in WhiteboardActivity

--- app/src/main/java/com/llc/drawit/presentation/recyclerView/MembersAdapter.java
+++ app/src/main/java/com/llc/drawit/presentation/recyclerView/MembersAdapter.java
@@ -1,5 +1,7 @@
 package com.llc.drawit.presentation.recyclerView;
 
+import android.content.res.ColorStateList;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.llc.drawit.R;
+import com.llc.drawit.databinding.MemberImageItemLayoutBinding;
 import com.llc.drawit.domain.entities.User;
+import com.llc.drawit.presentation.util.StringUtil;
+import com.llc.drawit.presentation.util.color.DefaultProfileImageColorsUtil;
 
 import java.util.List;
 
@@ -17,7 +22,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.ViewHolder> {
 
-    private List<User> members;
+    private final List<User> members;
 
     public MembersAdapter(List<User> members) {
         this.members = members;
@@ -31,16 +36,13 @@ public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.member_image_item_layout, parent, false);
-        return new ViewHolder(view);
+        MemberImageItemLayoutBinding binding = MemberImageItemLayoutBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String memberProfileImageUrl = members.get(position).getProfileImageUrl();
-
-        if (!memberProfileImageUrl.isEmpty())
-            Glide.with(holder.itemView.getContext()).load(memberProfileImageUrl).into(holder.memberProfileIv);
+    public void onBindViewHolder(@NonNull MembersAdapter.ViewHolder holder, int position) {
+        holder.bind(members.get(position));
     }
 
     @Override
@@ -48,14 +50,37 @@ public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.ViewHold
         return members.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView memberProfileIv;
+        private MemberImageItemLayoutBinding binding;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ViewHolder(@NonNull MemberImageItemLayoutBinding binding) {
+            super(binding.getRoot());
 
-            memberProfileIv = itemView.findViewById(R.id.iv_member_avatar);
+            this.binding = binding;
+        }
+
+        public void bind(User member) {
+            // display the profile image
+            if (member.getProfileImageUrl().isEmpty()) {
+                binding.ivMemberAvatar.setVisibility(View.GONE);
+                binding.defaultProfileImage.setVisibility(View.VISIBLE);
+
+                // we want to display the initials of the user
+                Pair<Integer, Integer> randomColorsPair = DefaultProfileImageColorsUtil.getRandomColorsPair();
+                binding.defaultProfileImage.setBackgroundTintList(ColorStateList.valueOf(
+                        binding.getRoot().getContext().getColor(randomColorsPair.first)
+                ));
+                binding.defaultProfileImage.setTextColor(binding.getRoot().getContext().getColor(randomColorsPair.second));
+
+                // display the initials
+                binding.defaultProfileImage.setText(StringUtil.getInitials(member.getName()));
+            }
+            else {
+                Glide.with(binding.getRoot().getContext()).load(member.getProfileImageUrl()).into(binding.ivMemberAvatar);
+                binding.ivMemberAvatar.setVisibility(View.VISIBLE);
+                binding.defaultProfileImage.setVisibility(View.GONE);
+            }
         }
     }
 }
